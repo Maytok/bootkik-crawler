9806dad8eae9d35f05d2be028c9524951f71a4b5
Giancarlo Ventura <khan.maytok@gmail.com>
2018-12-04 12:44:03 -0500

    asdasdawsd

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('*', async (req, res) => {
 
         const local_url = req.originalUrl;
         const request_url = req.query.page
-        console.log(request_url);
-        await page.goto(request_url, {
+        console.log(local_url);
+        await page.goto(local_url, {
             waitUntil: "networkidle0",
         });
 
